2543a2c05fd34d19ab1b7e52864ba00387011a66
Min-Qi-Zhang <minqi0303@gmail.com>
2022-04-07 00:37:46 -0400

    community page draft

--- keyboard-music/src/pages/Community/index.js
+++ keyboard-music/src/pages/Community/index.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import { Card, Button } from 'react-bootstrap';
+import network from "../../helpers/network";
 
-// import "./index.css";
+import "./index.css";
 
 class CommunityPage extends Component {
+  state = {
+    records: [],
+    page: 1
+  };
+
+  getPublishedRecords = () => {
+    network(
+      "query",
+      `getPublishedRecordsByPage(page: ${this.state.page})`,
+      `_id
+      title`,
+      window.sessionStorage.getItem("token")
+    ).then((res) => {
+      if (res.data) {
+        this.setState({ records: res.data.getPublishedRecordsByPage });
+        // console.log(this.state.records);
+      }
+    })
+  }
+
+  componentDidMount = () => {
+    this.getPublishedRecords();
+  }
+
   render() {
-    return <div className="me"></div>;
+    return (
+    <div className="me">
+      {this.state.records.map((record, index) => {
+        return (
+          <Card key={index} className="card">
+            <Card.Title className="card-title">{record.title}</Card.Title>
+            <Button>Play</Button>
+          </Card>
+        );
+      })}
+    </div>);
   }
 }
 
